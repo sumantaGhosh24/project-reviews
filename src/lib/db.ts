13e763d661be05062d6e970371ea77465f64cc68
sumanta ghosh <84187999+sumantaGhosh24@users.noreply.github.com>
2026-01-08 19:34:43 +0530

    add neon database using prisma orm

---
+++ src/lib/db.ts
@@ -0,0 +1,16 @@
+import {PrismaNeon} from "@prisma/adapter-neon";
+
+import {PrismaClient} from "@/generated/prisma/client";
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const adapter = new PrismaNeon({connectionString: process.env.DATABASE_URL});
+const prisma = globalForPrisma.prisma || new PrismaClient({adapter});
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
+
+export default prisma;
